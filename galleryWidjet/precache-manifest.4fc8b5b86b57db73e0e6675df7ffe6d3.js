3853556bb04aba0a1345433a5645b0fcd9346dc3
Roman <54660879+code-rnd@users.noreply.github.com>
2019-12-07 18:23:27 +0500

    Add files via upload

---
+++ galleryWidjet/precache-manifest.4fc8b5b86b57db73e0e6675df7ffe6d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fece5f99b3cdb7daef28dd3bd0d7ed83",
+    "url": "./index.html"
+  },
+  {
+    "revision": "738881d2b8ebce5b8d94",
+    "url": "./static/css/main.195f4419.chunk.css"
+  },
+  {
+    "revision": "cf0669813338ac609911",
+    "url": "./static/js/2.3c67f8ae.chunk.js"
+  },
+  {
+    "revision": "738881d2b8ebce5b8d94",
+    "url": "./static/js/main.b1da4e60.chunk.js"
+  },
+  {
+    "revision": "e650a0a6be1c21057545",
+    "url": "./static/js/runtime-main.5ecba51f.js"
+  }
+]);
