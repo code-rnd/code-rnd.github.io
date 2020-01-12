e2db5a26c701802f69922edf86fbcf6b18f429ee
Roman <54660879+code-rnd@users.noreply.github.com>
2020-01-13 03:08:12 +0500

    todolist commit

---
+++ toDoList/precache-manifest.125eade15c7fdfa90b47050623a88237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90c0e560ef50febf69318e5ac1819f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8d3fe79556928eaf208",
+    "url": "./static/css/main.11ed3554.chunk.css"
+  },
+  {
+    "revision": "04f775209c3ae4c8af15",
+    "url": "./static/js/2.00bce3c2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.00bce3c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8d3fe79556928eaf208",
+    "url": "./static/js/main.b276ead1.chunk.js"
+  },
+  {
+    "revision": "fba707398b6f9ac6cb98",
+    "url": "./static/js/runtime-main.3e5ab8e8.js"
+  }
+]);
