f6881262b2334da80c9b979d458afc9d09b02f48
Roman <54660879+code-rnd@users.noreply.github.com>
2020-02-05 01:08:22 +0500

    Add files via upload

---
+++ fortune-game-app/precache-manifest.c727741850615f4fa472c3fc060f729e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9069e1019ae652068ac323e872f45a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2fe93103b8b025381eec",
+    "url": "./static/css/main.1d433b71.chunk.css"
+  },
+  {
+    "revision": "f8ec5878e0b8d91e5cd7",
+    "url": "./static/js/2.52275d6c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.52275d6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fe93103b8b025381eec",
+    "url": "./static/js/main.ab93595c.chunk.js"
+  },
+  {
+    "revision": "9c69c9a2311d98750740",
+    "url": "./static/js/runtime-main.6faec372.js"
+  }
+]);
