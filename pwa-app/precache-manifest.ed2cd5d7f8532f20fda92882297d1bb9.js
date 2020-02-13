12b242fa976e160373fa3563cf45779c7c6ffa45
Roman <54660879+code-rnd@users.noreply.github.com>
2020-02-13 14:42:50 +0500

    Add files via upload

---
+++ pwa-app/precache-manifest.ed2cd5d7f8532f20fda92882297d1bb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1905c53328e2e4b51dd08b9abb1a07e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f25c6eaeccaf6942bf3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7a2ea6950455b4fec1a3",
+    "url": "/static/js/2.b719b4ec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b719b4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f25c6eaeccaf6942bf3",
+    "url": "/static/js/main.616475db.chunk.js"
+  },
+  {
+    "revision": "cf85f3956ab06177e612",
+    "url": "/static/js/runtime-main.36b73da1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
