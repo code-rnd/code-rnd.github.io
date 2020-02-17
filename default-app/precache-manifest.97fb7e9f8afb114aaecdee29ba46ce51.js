cac6b05c7221fc43f085938fb759e67d955d2d82
Roman <54660879+code-rnd@users.noreply.github.com>
2020-02-17 23:29:32 +0500

    Add files via upload

---
+++ default-app/precache-manifest.97fb7e9f8afb114aaecdee29ba46ce51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7354d58e0546f72b2d0ea1b1802a51c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "310168422def4713de24",
+    "url": "./static/css/main.485ce861.chunk.css"
+  },
+  {
+    "revision": "ee5269a22a2db824587d",
+    "url": "./static/js/2.0e2eb16a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.0e2eb16a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "310168422def4713de24",
+    "url": "./static/js/main.838d9d73.chunk.js"
+  },
+  {
+    "revision": "fa1eb949f2a0dcec2508",
+    "url": "./static/js/runtime-main.a140423a.js"
+  }
+]);
