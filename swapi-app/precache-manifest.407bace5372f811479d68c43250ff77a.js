d2c30a409fdf2e960a41f337125f569ae67b521c
Roman <54660879+code-rnd@users.noreply.github.com>
2020-02-19 17:47:27 +0500

    Add files via upload

---
+++ swapi-app/precache-manifest.407bace5372f811479d68c43250ff77a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8e33d3db7973a9109cb633a5a61485",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dd0bc842d2cc52420ee3",
+    "url": "./static/css/main.d33b238f.chunk.css"
+  },
+  {
+    "revision": "34ea8f028b06b79842b6",
+    "url": "./static/js/2.393c4fbc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.393c4fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0bc842d2cc52420ee3",
+    "url": "./static/js/main.99f4c5ec.chunk.js"
+  },
+  {
+    "revision": "df030eaf7d7da0fe7632",
+    "url": "./static/js/runtime-main.47ffa52a.js"
+  }
+]);
