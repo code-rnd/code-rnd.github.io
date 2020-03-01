a3a7c2b0ec9f3598c63edf8edccd4a1d7e02096a
Roman <54660879+code-rnd@users.noreply.github.com>
2020-03-01 22:32:29 +0500

    Add files via upload

---
+++ weather-app/precache-manifest.8913153aee043c33cd0e65021755e177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2465cc8c7f6ee9cacefa75d1c0992a1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c6a41e28c713a99db86",
+    "url": "./static/css/main.0c05bb44.chunk.css"
+  },
+  {
+    "revision": "f9e8d80f5ac0d8c3f9db",
+    "url": "./static/js/2.7d1ba2b2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.7d1ba2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6a41e28c713a99db86",
+    "url": "./static/js/main.0b14fd53.chunk.js"
+  },
+  {
+    "revision": "7f2151d4c8b2b650c938",
+    "url": "./static/js/runtime-main.f232b196.js"
+  }
+]);
