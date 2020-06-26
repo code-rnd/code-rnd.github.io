1a3b4d10764654f6e2da57365a7cf6b78d5fc18b
Roman <54660879+code-rnd@users.noreply.github.com>
2020-06-26 17:50:28 +0500

    Add files via upload

--- magic/service-worker.js
+++ magic/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9880a4d6a2428520182ce7b4bfc4057a.js"
+  "./precache-manifest.c4f98a1e654d560c23d4f4b106979e3d.js"
 );
 
 self.addEventListener('message', (event) => {
