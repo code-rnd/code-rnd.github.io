3db83ed112cbf835f91830d0522ee5aa5a31aea3
Roman <54660879+code-rnd@users.noreply.github.com>
2020-06-26 14:41:30 +0500

    Add files via upload

---
+++ magic/precache-manifest.bd587f40b06de8c57ae49970e948f986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e6ac036087a7f0434a41ffebc021ed",
+    "url": "/code-rnd/index.html"
+  },
+  {
+    "revision": "a793ad940e7fb0333de7",
+    "url": "/code-rnd/static/css/main.9c7c3323.chunk.css"
+  },
+  {
+    "revision": "d2ff9db4dc40a00f14e8",
+    "url": "/code-rnd/static/js/2.9578e521.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/code-rnd/static/js/2.9578e521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a793ad940e7fb0333de7",
+    "url": "/code-rnd/static/js/main.8d8e7c53.chunk.js"
+  },
+  {
+    "revision": "eee4d9ae17dba9184633",
+    "url": "/code-rnd/static/js/runtime-main.151a6223.js"
+  }
+]);
