9982c072052e86e062d0dc9b50e0caa008beb830
Roman <54660879+code-rnd@users.noreply.github.com>
2020-10-12 23:52:55 +0500

    Add files via upload

---
+++ panel-administrator/precache-manifest.ca57f817c9aa5842be565bec6cb4a6a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c441bbdcaad44b9160022961cf04a7c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97c12a2f8a7fb6fcecfe",
+    "url": "./static/css/main.fdc6ffbd.chunk.css"
+  },
+  {
+    "revision": "6854786784cb9842792a",
+    "url": "./static/js/2.be5ac3f4.chunk.js"
+  },
+  {
+    "revision": "f08e720869e7a847549fb58c8267edec",
+    "url": "./static/js/2.be5ac3f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c12a2f8a7fb6fcecfe",
+    "url": "./static/js/main.b1517943.chunk.js"
+  },
+  {
+    "revision": "5cd3bad510646bd9248e",
+    "url": "./static/js/runtime-main.0b8b72bc.js"
+  },
+  {
+    "revision": "462276b8ac9a920b735a15dbd9aadc5e",
+    "url": "./static/media/plus-icon.462276b8.svg"
+  },
+  {
+    "revision": "18320dd1cd8c9c1c1ca6bd2437eef75d",
+    "url": "./static/media/spinner-icon.18320dd1.svg"
+  }
+]);
