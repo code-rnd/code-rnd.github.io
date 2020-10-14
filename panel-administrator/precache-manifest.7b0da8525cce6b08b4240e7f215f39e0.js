533945ee24c21a891e4198028471cefab36ba157
Roman <54660879+code-rnd@users.noreply.github.com>
2020-10-14 23:26:42 +0500

    Add files via upload

---
+++ panel-administrator/precache-manifest.7b0da8525cce6b08b4240e7f215f39e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61334c1447f02fb27d88f782ccb19ecd",
+    "url": "/panel-administrator/index.html"
+  },
+  {
+    "revision": "4f14c553baa84cfac43f",
+    "url": "/panel-administrator/static/css/main.b0d13c16.chunk.css"
+  },
+  {
+    "revision": "5b68f059b5771237f7c7",
+    "url": "/panel-administrator/static/js/2.9c965de5.chunk.js"
+  },
+  {
+    "revision": "f08e720869e7a847549fb58c8267edec",
+    "url": "/panel-administrator/static/js/2.9c965de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f14c553baa84cfac43f",
+    "url": "/panel-administrator/static/js/main.2d68fb1c.chunk.js"
+  },
+  {
+    "revision": "a64961aec872e9eeca78",
+    "url": "/panel-administrator/static/js/runtime-main.719a411f.js"
+  },
+  {
+    "revision": "462276b8ac9a920b735a15dbd9aadc5e",
+    "url": "/panel-administrator/static/media/plus-icon.462276b8.svg"
+  },
+  {
+    "revision": "18320dd1cd8c9c1c1ca6bd2437eef75d",
+    "url": "/panel-administrator/static/media/spinner-icon.18320dd1.svg"
+  }
+]);
